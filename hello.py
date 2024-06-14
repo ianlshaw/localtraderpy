58a98092748332b880edd3895395b1e2773fde7a
ianlshaw <ian.shaw1991@gmail.com>
2024-06-14 15:06:37 +0100

    Added get request to spacetraders api to look for marketplaces given a system_symbol

--- hello.py
+++ hello.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask import request
+import json
+import requests
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, CD-TEST-3!'
+@app.route('/localtrader/<system_symbol>')
+def create(system_symbol=None):
+
+  marketplaces_url = "https://api.spacetraders.io/v2/systems/" + system_symbol + "/waypoints?traits=MARKETPLACE"
+
+  r = requests.get(marketplaces_url)
+
+  return r.text
+
+
+
